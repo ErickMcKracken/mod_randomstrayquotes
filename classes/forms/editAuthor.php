e1fefc2fe6d2cc27664011f045eef930f83e27ce
Eric Frenette <frenette.eric@uqam.ca>
2018-06-28 09:47:02 -0400

    Finalisation du formulaire update_author avec upload de fichier.

--- classes/forms/editAuthor.php
+++ classes/forms/editAuthor.php
@@ -22,18 +22,15 @@ class editAuthor extends \moodleform {
         $authorid = $customdata['authorid'];//required_param('authorid', PARAM_INT);
         $queryauth = "Select distinct * from {randomstrayquotes_authors} where id = $authorid";
         $author = $DB->get_record_sql($queryauth);
-// Texbox to edit an author
+        // Texbox to edit an author
         $attributes = array('size' => '50', 'maxlength' => '100');
         $mform->addElement('text', 'author_name', get_string('author_name', 'mod_randomstrayquotes'), $attributes);
         $mform->addRule('author_name', null, 'required', null, 'client');
         $mform->setDefault('author_name', $author->author_name);
         $mform->setType('author_name', PARAM_TEXT);
 
-
-        if (empty($entry->id)) {
-            $entry = new \stdClass;
-            $entry->id = null;
-        }
+        $entry = new \stdClass;
+        $entry->id = $customdata['photofile']->get_itemid();
 
         $draftitemid = file_get_submitted_draft_itemid('userfile');
         $maxbytes = '50000';
@@ -42,13 +39,10 @@ class editAuthor extends \moodleform {
                 $customdata['ctx']->id, 
                 'mod_randomstrayquotes', 
                 'content', 
-                $customdata['photofile']->get_id(), 
+                $entry->id, 
                 array('subdirs' => 0, 'maxbytes' => $maxbytes, 'maxfiles' => 50)
                 );
-
-
-// Edit picture
-//$mform->addElement('filepicker', 'userfile', get_string('AuthorPicture', 'mod_randomstrayquotes'), null, array('maxbytes' => $maxbytes, 'accepted_types' => '.jpg', '.gif', '.png'));
+        $entry->userfile = $draftitemid;
 
         $mform->addElement('filemanager', 'userfile', get_string('AuthorPicture', 'mod_randomstrayquotes'), null, [
             'subdirs' => 0,
@@ -60,33 +54,33 @@ class editAuthor extends \moodleform {
         );
         $entry->userfile = $draftitemid;
 
-        $mform->setDefault('userfile', $draftitemid);
+        //$mform->setDefault('userfile', $draftitemid);
 
-// Indicate the user_id
+        // Indicate the user_id
         $mform->addElement('hidden', 'user_id', "$author->user_id");
         $mform->setType('user_id', PARAM_INT);
 
         $mform->addElement('hidden', 'authorid', "$authorid");
         $mform->setType('authorid', PARAM_INT);
 
-// Indicate the courseid
-        $mform->addElement('hidden', 'course_id', "$author->course_id");
-        $mform->setType('course_id', PARAM_INT);
-//$mform->setDefault('course_id', $author->course_id);
-// Indicate the date of the add
-
+        // Indicate the courseid
+        $mform->addElement('hidden', 'courseid', "$author->course_id");
+        $mform->setType('courseid', PARAM_INT);
+        
+        // Indicate the date of the add
         $date = new \DateTime("now");
         $time = $date->format('Y-m-d_H.i');
 
         $mform->addElement('hidden', 'time_added', "$time");
         $mform->setType('time_added', PARAM_ALPHANUMEXT);
-//$mform->setDefault('time_added', $time); 
-// Put an array of buttons on the form
+        // Put an array of buttons on the form
         $buttonarray = array();
         $buttonarray[] = & $mform->createElement('submit', 'submitbutton', get_string('savechanges'));
-        $buttonarray[] = & $mform->createElement('submit', 'cancel', get_string('cancel'));
+        $buttonarray[] = & $mform->createElement('cancel');
         $buttonarray[] = & $mform->createElement('submit', 'delete', get_string('delete'), array('class' => 'btn btn-danger'));
         $mform->addGroup($buttonarray, 'buttonar', '', array(' '), false);
+        
+        $this->set_data($entry);
     }
 
 }
--- edit_author.php
+++ edit_author.php
@@ -4,7 +4,7 @@ require_once('../../config.php');
 //require_once ($CFG->dirroot.'/lib/formslib.php');
 //require_once($CFG->dirroot . '/mod/randomstrayquotes/locallib.php');
 
-global $CFG, $DB, $PAGE, $COURSE;
+global $CFG, $DB, $PAGE, $COURSE, $USER;
 
 $PAGE->set_context(context_system::instance());
 $PAGE->set_pagelayout('standard');
@@ -17,21 +17,26 @@ $customdata['authorid'] = required_param('authorid', PARAM_INT);
 
 $ctx = context_course::instance($course_id);
 $imageid = $DB->get_record('randomstrayquotes_authors', ['id' => $customdata['authorid']], 'author_picture');
-$imagePathHash = $DB->get_records('files', ['itemid' => $imageid->author_picture, 'component' => 'mod_randomstrayquotes', 'filearea' => 'content', 'filesize' => '> 0'], 'pathnamehash');
-$file_ids = array_keys($imagePathHash);
-$file = $imagePathHash[$file_ids[0]];
-$fs = get_file_storage();
-$fichier = $fs->get_file_by_hash($file->pathnamehash);
 
-$customdata['photofile'] = $fichier; 
+$fs = get_file_storage();
+$imagePathHash = $fs->get_area_files($ctx->id, 'mod_randomstrayquotes', 'content', $imageid->author_picture, "itemid, filepath, filename", false);
+$files = array_values($imagePathHash);
+
+$customdata['photofile'] = $files[0]; 
 $customdata['ctx'] = $ctx;
 
 $form = new \mod_randomstrayquotes\forms\editAuthor(null, $customdata);
 
-echo $OUTPUT->header();
-echo $OUTPUT->heading('Edit an author\'s informations');
+
 //TODO: valeur répeptée à mettre dans une seule place
 $maxbytes = '50000';
+if ($form->is_cancelled()) {
+    redirect(new moodle_url('/mod/randomstrayquotes/add_authors.php', ['courseid' => $course_id,  'userid' => $USER->id ]));
+}
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading('Edit an author\'s informations');
+
 if ($data = $form->get_data()) {
     // ... store or update $entry
     file_save_draft_area_files($data->userfile, $ctx->id, 'mod_randomstrayquotes', 'content',
